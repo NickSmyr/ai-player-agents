096f7f09e736a88f51e62ae8cbccff74b1f0b935
achariso <thanasis.charisoudis@gmail.com>
2021-09-22 12:19:42 +0200

    update Matrix2d and Vector classes to enable matrix-matrix, matrix-vector and vector-dot product operations
    Signed-off-by: achariso <thanasis.charisoudis@gmail.com>

--- A2 - HMM/hmm.py
+++ A2 - HMM/hmm.py
@@ -5,7 +5,7 @@ class HMM:
 
     def __init__(self, N: int, K: int):
         # Random intialization
-        # TODO: better initialize
+        # TODO: better initialization than uniform
         self.A = Matrix2d.random(N, N)
         self.A.normalize_rows()
         self.B = Matrix2d.random(N, K)
@@ -13,18 +13,31 @@ class HMM:
         self.pi = Vector.random(N)
         self.pi.normalize()
 
-    def alpha_pass(self, O: list) -> float:
+        # ------------------------------------
+        # Shape Tests:
+        # ------------------------------------
+        # C = (self.A @ self.B).T @ self.A.T
+        # print(type(C), C.shape)
+        # Cv = C @ Vector.random(4)
+        # print(type(Cv), Cv.shape)
+        # ------------------------------------
+        # All passed.
+        # ------------------------------------
+
+    def alpha_pass(self, Ot: list) -> float:
         """
         Perform a forward pass echoing the probability of an observation sequence.
-        P(Ο[1:T]) = P()
-
-        B * pi
-        :param list O: {Ot} for t=1...T, where Ot in {0, ..., K}
+        :param list Ot: {Ot} for t=1...T, where Ot in {0, ..., K}
         :return: a float object
         """
         # Initialize alpha
-        alpha = self.pi @ self.B.get_col(O[0])
+        alpha = self.pi @ self.B.get_col(Ot[0])
+        print(alpha)
+        # Perform alpha-pass iterations
+        # TODO
+        return alpha
 
 
 if __name__ == '__main__':
-    HMM(N=4, K=2)
+    hmm = HMM(N=4, K=2)
+    hmm.alpha_pass([0, 0, 1, 0])
